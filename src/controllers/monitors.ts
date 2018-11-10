6d267093925425814a710157536fef76f50803cb
Alec von Barnekow <von.barnekow.alec@gmail.com>
2018-11-10 15:57:50 +0100

    Improve typo

--- src/controllers/monitors.ts
+++ src/controllers/monitors.ts
@@ -3,9 +3,9 @@ import * as hapi from 'hapi';
 import * as mongoose from 'mongoose';
 
 import {ObjectID} from 'bson';
-import {MonitorSchema} from '../schemas/Monitors';
-import {Constants} from '../contants';
 import {Document} from 'mongoose';
+import {Constants} from '../contants';
+import {MonitorSchema} from '../schemas/Monitors';
 
 interface IFeedback {
   date: Date;
@@ -15,13 +15,13 @@ interface IFeedback {
 
 interface IMonitor extends Document {
 
-  user_id: mongoose.Schema.Types.ObjectId,
-  name: String,
-  is_public: Boolean,
-  address: String,
-  frequency: Number,
-  status: Number,
-  feedbacks: [IFeedback],
+  user_id: mongoose.Schema.Types.ObjectId;
+  name: string;
+  is_public: boolean;
+  address: string;
+  frequency: number;
+  status: number;
+  feedbacks: [IFeedback];
 }
 
 const Monitor = mongoose.model('monitors', MonitorSchema);
