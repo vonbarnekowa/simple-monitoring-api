23fe6ca1f9dd620f1e911f90151d304856fdf4f1
Alec von Barnekow <von.barnekow.alec@gmail.com>
2018-11-10 15:51:54 +0100

    Add mongoose

--- src/contants.ts
+++ src/contants.ts
@@ -1,2 +1,4 @@
 export class Constants {
+  // Mongodb
+  public static MONGODB_CONNECT_URL = 'mongodb://localhost/smw';
 }
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,16 @@
 import * as Hapi from 'hapi';
+import * as mongoose from 'mongoose';
 
 import {Constants} from '../contants';
 import {config} from './config';
 import {log} from './log';
 import {routes} from './routes';
+
+mongoose.connect(Constants.MONGODB_CONNECT_URL, {useNewUrlParser: true}, (error: mongoose.Error) => {
+  if (error) {
+    return log.error(error);
+  }
+});
 
 const server = new Hapi.Server({
   port: config.port,
